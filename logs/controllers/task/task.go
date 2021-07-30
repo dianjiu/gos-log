103e88fa63a0f466a42d18c2f5d04de33c5ec0e5
dianjiu <dianjiu@dianjiu.cc>
2021-07-30 15:25:49 +0800

    服务端调用logc的checkOn接口

--- logs/controllers/task/task.go
+++ logs/controllers/task/task.go
@@ -5,11 +5,21 @@ import (
 	"time"
 
 	"github.com/astaxie/beego"
+	"github.com/astaxie/beego/httplib"
 	"github.com/astaxie/beego/toolbox"
 )
 
 func task() error {
 	log.Println("定时检测客户端是否在线 Time:", time.Now().Format("2006-01-02 15:04:05"))
+	// TODO 查询所有客户端，协程调用客户端接口
+	var server string = "127.0.0.1:2020"
+	req := httplib.Get("http://" + server + "/checkOn")
+	str, err := req.String()
+	if err != nil {
+		log.Printf("check logc Online status err: %v", err)
+	}
+	log.Printf("check logc Online status ok: %v", str)
+	// TODO 根据状态，修改数据库标识
 	return nil
 }
 
