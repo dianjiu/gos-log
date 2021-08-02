8c92d4f59b7258be419ed2c110106d823ecdb721
dianjiu <dianjiu@dianjiu.cc>
2021-08-02 10:51:33 +0800

    logs支持客户端活性检测

--- logs/controllers/task/task.go
+++ logs/controllers/task/task.go
@@ -1,7 +1,10 @@
 package task
 
 import (
+	"encoding/json"
+	"errors"
 	"log"
+	"logs/models"
 	"time"
 
 	"github.com/astaxie/beego"
@@ -9,17 +12,48 @@ import (
 	"github.com/astaxie/beego/toolbox"
 )
 
+type Resp struct {
+	//必须的大写开头
+	Code string `json:"code"`
+	Msg  string `json:"msg"`
+}
+
 func task() error {
 	log.Println("定时检测客户端是否在线 Time:", time.Now().Format("2006-01-02 15:04:05"))
-	// TODO 查询所有客户端，协程调用客户端接口
-	var server string = "127.0.0.1:2020"
-	req := httplib.Get("http://" + server + "/checkOn")
-	str, err := req.String()
+	//TODO 查询所有客户端，协程调用客户端接口
+	clients, err := models.QueryAllClient()
 	if err != nil {
-		log.Printf("check logc Online status err: %v", err)
+		log.Printf("orm query all client err: %v", err)
+		return errors.New("ORM queryAllClient method is Exception")
 	}
-	log.Printf("check logc Online status ok: %v", str)
-	// TODO 根据状态，修改数据库标识
+	for _, client := range clients {
+		logcServer := client.Ip + ":" + client.Port
+		req := httplib.Get("http://" + logcServer + "/checkOn")
+		str, err := req.String()
+		if err != nil {
+			log.Printf("check logc Online status err: %v", err)
+			//return errors.New("HTTP request detection client status is Exception")
+		}
+		log.Printf("check logc Online status ok: %v", str)
+		var resp Resp
+		//字符串转结构体
+		json.Unmarshal([]byte(str), &resp)
+		if "200" == resp.Code {
+			log.Printf("check logc Online status resp code: %v", resp.Code)
+			// 根据状态，修改数据库标识
+			c := models.TClient{}
+			c.Id = client.Id
+			c.Online = "1"
+			models.ChangeClientOnline(&c)
+		} else {
+			log.Printf("check logc Online status resp code: %v", resp.Code)
+			// 根据状态，修改数据库标识
+			c := models.TClient{}
+			c.Id = client.Id
+			c.Online = "0"
+			models.ChangeClientOnline(&c)
+		}
+	}
 	return nil
 }
 
